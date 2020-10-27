c6ffbd85645e99536a2cf23b4f72b6e9c96ff006
Diego Rodriguez <lexelEZ@gmail.com>
2020-10-26 21:01:59 -0500

    Implemented invoice manager

--- src/views/Dashboard/InvoiceManager.vue
+++ src/views/Dashboard/InvoiceManager.vue
@@ -1,168 +1,268 @@
 <template>
-  <q-page class="q-pa-lg">
-    <div class="row q-mb-lg">
-      <div class="text-h5">
-        <div class="col q-px-md">Gestor de facturas</div>
-      </div>
-    </div>
-    <div class="row q-mb-lg">
-      <q-space />
-      <div class="col-lg-2 q-px-md">
-        <q-input dense filled label="Factura" type="number" v-model="searchInvoice" />
-      </div>
-      <div class="col-lg-2 q-px-md">
-        <q-input dense filled label="Casillero" v-model="searchBox" />
-      </div>
-      <div class="col-lg-2 q-px-md">
-        <q-input filled mask="date" label="Fecha" dense v-model="searchDate">
-          <template v-slot:append>
-            <q-icon name="fas fa-calendar" class="cursor-pointer">
-              <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
-                <q-date v-model="searchDate">
-                  <div class="row items-center justify-end">
-                    <q-btn v-close-popup label="Close" color="primary" flat />
-                  </div>
-                </q-date>
-              </q-popup-proxy>
-            </q-icon>
-          </template>
-        </q-input>
-      </div>
-      <div class="col-lg-1 q-px-md">
-        <q-btn color="primary" label="Buscar" />
-      </div>
-    </div>
-    <div class="row q-mb-xl">
-      <div class="col q-px-md">
-        <q-table
-          :data="invoicesData"
-          :columns="invoicesColumns"
-          row-key="name"
-          :pagination.sync="initialPagination"
-          class="full-width"
-          title="Facturas"
-        >
-          <template v-slot:header="props">
-            <q-tr :props="props">
-              <q-th v-for="col in props.cols" :key="col.name" :props="props">{{ col.label }}</q-th>
-              <q-th>Reenviar</q-th>
-            </q-tr>
-          </template>
+    <q-page class="q-pa-lg">
+        <div class="row q-mb-lg">
+            <div class="text-h5">
+                <div class="col q-px-md">Gestor de facturas</div>
+            </div>
+        </div>
+        <div class="row q-mb-lg">
+            <q-space />
+            <div class="col-lg-2 q-px-md">
+                <q-input
+                    dense
+                    filled
+                    label="Factura"
+                    type="number"
+                    v-model="searchInvoice"
+                />
+            </div>
+            <div class="col-lg-2 q-px-md">
+                <q-input dense filled label="Casillero" v-model="searchBox" />
+            </div>
+            <div class="col-lg-2 q-px-md">
+                <q-input
+                    filled
+                    mask="date"
+                    label="Fecha"
+                    dense
+                    v-model="searchDate"
+                >
+                    <template v-slot:append>
+                        <q-icon name="fas fa-calendar" class="cursor-pointer">
+                            <q-popup-proxy
+                                ref="qDateProxy"
+                                transition-show="scale"
+                                transition-hide="scale"
+                            >
+                                <q-date v-model="searchDate">
+                                    <div class="row items-center justify-end">
+                                        <q-btn
+                                            v-close-popup
+                                            label="Close"
+                                            color="primary"
+                                            flat
+                                        />
+                                    </div>
+                                </q-date>
+                            </q-popup-proxy>
+                        </q-icon>
+                    </template>
+                </q-input>
+            </div>
+            <div class="col-lg-1 q-px-md">
+                <q-btn
+                    color="primary"
+                    label="Buscar"
+                    @click="filterContent()"
+                />
+            </div>
+        </div>
+        <div class="row q-mb-xl">
+            <div class="col q-px-md">
+                <q-table
+                    :data="filterInvoicesData"
+                    :columns="invoicesColumns"
+                    row-key="name"
+                    :pagination.sync="initialPagination"
+                    class="full-width"
+                    title="Facturas"
+                >
+                    <template v-slot:header="props">
+                        <q-tr :props="props">
+                            <q-th
+                                v-for="col in props.cols"
+                                :key="col.name"
+                                :props="props"
+                                >{{ col.label }}</q-th
+                            >
+                            <q-th>Reenviar</q-th>
+                        </q-tr>
+                    </template>
 
-          <template v-slot:body="props">
-            <q-tr :props="props">
-              <q-td v-for="col in props.cols" :key="col.name" :props="props">{{ col.value }}</q-td>
-              <q-td auto-width>
-                <q-btn size="sm" color="primary" round dense icon="fas fa-envelope" flat />
-              </q-td>
-            </q-tr>
-          </template>
-        </q-table>
-      </div>
-    </div>
-  </q-page>
+                    <template v-slot:body="props">
+                        <q-tr :props="props">
+                            <q-td key="No" :props="props">
+                                {{ props.row.No }}
+                            </q-td>
+                            <q-td key="box" :props="props">
+                                {{ props.row.box }}
+                            </q-td>
+                            <q-td key="price" :props="props">
+                                {{ props.row.price }}
+                            </q-td>
+                            <q-td key="packages" :props="props">
+                                {{ returnPackagesAmount(props.row.packages) }}
+                            </q-td>
+
+                            <q-td key="date" :props="props">
+                                {{ returnFormatedTime(props.row.creationTime) }}
+                            </q-td>
+
+                            <q-td auto-width>
+                                <q-btn
+                                    size="sm"
+                                    color="primary"
+                                    round
+                                    dense
+                                    icon="fas fa-envelope"
+                                    flat
+                                />
+                            </q-td>
+                        </q-tr>
+                    </template>
+                </q-table>
+            </div>
+        </div>
+    </q-page>
 </template>
 
 <script>
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import * as api from '@/api/api'
+import moment from 'moment'
 export default {
-  data() {
-    return {
-      searchBox: "",
-      searchInvoice: "",
-      searchDate: "",
-      initialPagination: {
-        sortBy: "desc",
-        descending: false,
-        page: 1,
-        rowsPerPage: 10,
-      },
-      invoicesColumns: [
-        {
-          name: "invoice",
-          align: "left",
-          label: "Factura",
-          field: "invoice",
-          sortable: true,
+    data() {
+        return {
+            searchBox: '',
+            searchInvoice: '',
+            searchDate: '',
+            initialPagination: {
+                sortBy: 'desc',
+                descending: false,
+                page: 1,
+                rowsPerPage: 10,
+            },
+            invoicesColumns: [
+                {
+                    name: 'No',
+                    align: 'left',
+                    label: 'Factura',
+                    field: 'No',
+                    sortable: true,
+                },
+                {
+                    name: 'box',
+                    align: 'left',
+                    label: 'Casillero',
+                    field: 'box',
+                    sortable: true,
+                },
+                {
+                    name: 'price',
+                    align: 'left',
+                    label: 'Monto ($)',
+                    field: 'price',
+                    sortable: true,
+                },
+                {
+                    name: 'packages',
+                    align: 'left',
+                    label: 'Paquetes (cant.)',
+                    field: 'packages',
+                    sortable: true,
+                },
+                {
+                    name: 'date',
+                    align: 'left',
+                    label: 'Facturado el',
+                    field: 'date',
+                    sortable: true,
+                },
+            ],
+            invoicesData: [],
+            filterInvoicesData: [],
+        }
+    },
+    methods: {
+        clear() {
+            this.searchBox = ''
+            this.searchInvoice = ''
+            this.searchDate = ''
         },
-        {
-          name: "box",
-          align: "left",
-          label: "Casillero",
-          field: "box",
-          sortable: true,
+        filterContent() {
+            if (
+                this.searchBox === '' &&
+                this.searchInvoice === '' &&
+                this.searchDate === ''
+            )
+                this.filterInvoicesData = this.invoicesData
+            if (this.searchBox) {
+                this.filterInvoicesData = this.invoicesData.filter(invoice =>
+                    invoice.box
+                        .toLowerCase()
+                        .includes(this.searchBox.toLowerCase())
+                )
+            }
+            if (this.searchInvoice) {
+                this.filterInvoicesData = this.invoicesData.filter(invoice =>
+                    invoice.No.toString().includes(this.searchInvoice)
+                )
+            }
+            if (this.searchDate) {
+                this.filterInvoicesData = this.invoicesData.filter(packages => {
+                    let dataDate = moment(packages.creationTime).format(
+                        'YYYY/MM/DD'
+                    )
+                    if (moment(dataDate).isSame(this.searchDate)) {
+                        return packages
+                    }
+                })
+            }
+
+            this.clear()
         },
-        {
-          name: "amount",
-          align: "left",
-          label: "Monto ($)",
-          field: "amount",
-          sortable: true,
+        returnFormatedTime(time) {
+            return moment(time).format('DD/MM/YYYY')
         },
-        {
-          name: "packages",
-          align: "left",
-          label: "Paquetes (cant.)",
-          field: "packages",
-          sortable: true,
+        returnPackagesAmount(packages) {
+            return parseInt(packages.length)
         },
-        {
-          name: "date",
-          align: "left",
-          label: "Facturado el",
-          field: "date",
-          sortable: true,
+        addToData(id, data) {
+            data.id = id
+            this.invoicesData.push(data)
         },
-        {
-          name: "admin",
-          align: "left",
-          label: "Facturado por",
-          field: "admin",
-          sortable: true,
-        },        
-      ],
-      invoicesData: [
-        {
-          invoice: "1",
-          box: 'YGO-5687',
-          amount: 5.75,
-          packages: 2,
-          date: '1-6-2020',
-          admin: 'Pedro Picapiedras'
+        editData(id, data) {
+            data.id = id
+            this.invoicesData.forEach((d, index) => {
+                if (d.id === id) {
+                    this.invoicesData.splice(index, 1, data)
+                }
+            })
         },
-        {
-          invoice: "1",
-          box: 'YGO-5687',
-          amount: 5.75,
-          packages: 2,
-          date: '1-6-2020',
-          admin: 'Pedro Picapiedras'
+        removeData(id) {
+            this.invoicesData.forEach((d, index) => {
+                if (d.id === id) {
+                    this.invoicesData.splice(index, 1)
+                }
+            })
         },
-        {
-          invoice: "1",
-          box: 'YGO-5687',
-          amount: 5.75,
-          packages: 2,
-          date: '1-6-2020',
-          admin: 'Pedro Picapiedras'
+    },
+    watch: {
+        invoicesData(newValue, oldValue) {
+            this.filterInvoicesData = newValue
         },
-        {
-          invoice: "1",
-          box: 'YGO-5687',
-          amount: 5.75,
-          packages: 2,
-          date: '1-6-2020',
-          admin: 'Pedro Picapiedras'
-        },
-        {
-          invoice: "1",
-          box: 'YGO-5687',
-          amount: 5.75,
-          packages: 2,
-          date: '1-6-2020',
-          admin: 'Pedro Picapiedras'
-        },
-      ],
-    };
-  },
-};
+    },
+    mounted() {
+        let db = firebase.firestore()
+        db.collection('invoices').onSnapshot(
+            snapshot => {
+                snapshot.docChanges().forEach(change => {
+                    if (change.type === 'added') {
+                        this.addToData(change.doc.id, change.doc.data())
+                    }
+                    if (change.type === 'modified') {
+                        this.editData(change.doc.id, change.doc.data())
+                    }
+                    if (change.type === 'removed') {
+                        this.removeData(change.doc.id)
+                    }
+                })
+            },
+            error => {
+                console.log(error)
+            }
+        )
+    },
+}
 </script>
