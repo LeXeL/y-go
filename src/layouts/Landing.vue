f317a6424b2b508d60a79849f6b94a1c76dfe278
KidDarknezz <darkslave@live.com>
2020-11-05 13:37:13 -0500

    hero section

--- src/layouts/Landing.vue
+++ src/layouts/Landing.vue
@@ -1,8 +1,37 @@
 <template>
   <q-layout>
-    <div class="social-ribbon">
-      social
+    <!-- SOCIAL LINKS -->
+    <div class="social-ribbon bg-accent text-white">
+      <div class="row">
+        <div class="col desktop-only"></div>
+        <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12 q-px-md">
+          <ul>
+            <li><a href="#" target="_blank"><i class="fab fa-instagram q-mr-sm"></i>Instagram</a></li>
+            <li><a href="#" target="_blank"><i class="fab fa-facebook q-mr-sm"></i>Facebook</a></li>
+          </ul>
+        </div>
+        <div class="col desktop-only"></div>
+      </div>
     </div>
+    <!-- END SOCIAL LINKS -->
+
+    <!-- NAVIGATION BAR -->
+    <div class="navigation-bar bg-primary text-white shadow-5" style="margin-bottom: 50px;">
+      <div class="row">
+        <div class="col desktop-only"></div>
+        <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12 q-px-md">
+          <q-img :src="require('@/assets/logo_ygo.png')" style="width: 75px; margin-top: 7px;"/>
+          <ul>
+            <li><router-link to="/"><div class="text-h6">INICIO</div></router-link></li>
+            <li><router-link to="/"><div class="text-h6">TARIFAS</div></router-link></li>
+            <li><router-link to="/"><div class="text-h6">NOSOTROS</div></router-link></li>
+            <li><router-link to="/"><div class="text-h6">CONTACTO</div></router-link></li>
+          </ul>
+        </div>
+        <div class="col desktop-only"></div>
+      </div>
+    </div>
+    <!-- END NAVIGATION BAR -->
     <q-page-container>
       <router-view/>
     </q-page-container>
--- src/views/Landing/Home.vue
+++ src/views/Landing/Home.vue
@@ -1,3 +1,87 @@
 <template>
-  <p>landing home</p>
+  <q-page>
+    <div class="row q-py-xl">
+      <div class="col desktop-only"></div>
+      <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12 q-px-md">
+        <div class="row" style="margin-bottom: 50px;">
+          <div class="col-lg-6 col-xs-12 q-px-md">
+            <q-carousel
+              v-model="slide"
+              swipeable
+              animated
+              transition-prev="scale"
+              transition-next="scale"
+              control-type="flat"
+              control-color="accent"
+              infinite
+              arrows
+              style="height: 400px;"
+              class="text-accent rounded-borders"
+              autoplay
+            >
+              <q-carousel-slide v-for="(slide, i) in slidesContent" :key="i" :name="slide.name" class="column no-wrap flex-center">
+                <q-img :src="require(`@/assets/Landing/${slide.pic}`)" style="width: 30%; margin-bottom: 50px;"/>
+                <div class="text-subtitle2">{{ slide.subtitle }}</div>
+                <div class="text-h4 text-weight-bolder">{{ slide.title }}</div>
+                <div class="q-mt-md text-center text-black">
+                  {{ slide.text }}
+                </div>
+              </q-carousel-slide>
+            </q-carousel>
+          </div>
+          <div class="col-lg-6 col-xs-12 q-px-md">
+            <div class="bg-primary rounded-borders q-pa-lg text-white column flex-center">
+              <q-img :src="require('@/assets/logo_ygo.png')" style="width: 40%; margin: 40px 0;"/>
+              <div class="text-h4 text-center text-weight-bolder" style="margin: 10px 0;">ABRE TU CASILLERO GRATIS</div>
+              <div class="text-subtitle2">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</div>
+              <q-btn color="accent full-width q-mt-lg" push label="Registrate aqui" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col desktop-only"></div>  
+    </div>
+    <div class="row bg-grey-2 q-py-xl">
+      <div class="col desktop-only"></div>
+      <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12 q-px-md">
+        <div class="text-h4 text-weight-bolder text-center text-primary" style="height: 1000px;">
+          REGISTRATE
+        </div>
+      </div>
+      <div class="col desktop-only"></div>
+    </div>
+  </q-page>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      slide: 'slide_1',
+      slidesContent: [
+        {
+          name: 'slide_1',
+          pic: 'slider_pic_1.jpg',
+          title: 'This Is Title 1',
+          subtitle: 'This is subtitle 1',
+          text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum slide 1.'
+        },
+        {
+          name: 'slide_2',
+          pic: 'slider_pic_2.jpg',
+          title: 'This Is Title 2',
+          subtitle: 'This is subtitle 2',
+          text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum slide 2.'
+        },
+        {
+          name: 'slide_3',
+          pic: 'slider_pic_3.jpg',
+          title: 'This Is Title 3',
+          subtitle: 'This is subtitle 3',
+          text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum slide 3.'
+        },
+      ]
+    }
+  }
+}
+</script>
