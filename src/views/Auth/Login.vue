22e512019f1615269f7a59fb8a0321e29341f8fb
Alejandro Millan <darkslave@live.com>
2020-11-13 16:45:29 -0500

    error banner

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -1,6 +1,9 @@
 <template>
     <q-page class="bg-primary">
         <div class="absolute-center">
+            <q-banner dense inline-actions class="text-white bg-warning rounded-borders q-mb-md">
+                You have lost connection to the internet. This app is offline.
+            </q-banner>
             <q-card style="width: 400px" class="q-pa-md rounded-borders">
                 <q-card-section>
                     <q-img :src="require('@/assets/logo_ygo.png')" class="q-mb-md" />
