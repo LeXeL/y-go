194fde571fe638f172112feb82cc8dacb7118908
KidDarknezz <darkslave@live.com>
2020-11-30 21:32:26 -0500

    implement q-form

--- src/components/UsersDashboard/CompleteRegistrationForm.vue
+++ src/components/UsersDashboard/CompleteRegistrationForm.vue
@@ -1,140 +1,154 @@
 <template>
     <q-card>
-        <q-card-section class="text-center">
-            <i class="far fa-laugh-wink fa-5x text-primary"></i>
-        </q-card-section>
-        <q-card-section class="text-center">
-            <div class="text-h5 text-bold text-accent">¡Bienvenido!</div>
-        </q-card-section>
-        <q-card-section>
-            <div class="row">
-                <div class="col desktop-only"></div>
-                <div class="col-lg-5">
-                    <div class="text-subtitle1 text-center q-mb-lg">
-                        Para continuar a tu casillero, porfavor termina de llenar la informacion
-                        personal de tu cuenta.
-                    </div>
-                    <div class="row q-mb-md">
-                        <div class="col">
-                            <q-input
-                                filled
-                                label="Nombre"
-                                class="on-left"
-                                v-model="registrationData.name"
-                            />
+        <q-form @submit="submitForm()">
+            <q-card-section class="text-center">
+                <i class="far fa-laugh-wink fa-5x text-primary"></i>
+            </q-card-section>
+            <q-card-section class="text-center">
+                <div class="text-h5 text-bold text-accent">¡Bienvenido!</div>
+            </q-card-section>
+            <q-card-section>
+                <div class="row">
+                    <div class="col desktop-only"></div>
+                    <div class="col-lg-5">
+                        <div class="text-subtitle1 text-center q-mb-lg">
+                            Para continuar a tu casillero, porfavor termina de llenar la informacion
+                            personal de tu cuenta.
                         </div>
-                        <div class="col">
-                            <q-input
-                                filled
-                                label="Apellido"
-                                class="on-right"
-                                v-model="registrationData.lastName"
-                            />
-                        </div>
-                    </div>
-                    <div class="row q-mb-md">
-                        <div class="col">
-                            <q-input filled label="Telefono" v-model="registrationData.phone" />
-                        </div>
-                    </div>
-                    <div class="row q-mb-md">
-                        <div class="col">
-                            <q-select filled label="Sucursal de preferencia" :options="['Sucursal A', 'Sucursal B']"/>
-                        </div>
-                    </div>
-                    <div class="row q-mb-md">
-                        <div class="col-lg-4 col-xs-12 q-pa-sm">
-                            <div
-                                :class="
-                                    registrationData.selectedRateId == 'plan_basico_id'
-                                        ? 'rateTileSelected q-pa-md rounded-borders text-center'
-                                        : 'rateTile q-pa-md rounded-borders text-center'
-                                "
-                                @click="registrationData.selectedRateId = 'plan_basico_id'"
-                            >
-                                <i class="fas fa-weight fa-2x q-mb-sm"></i>
-                                <div class="text-h6 q-mb-sm text-bold">Plan Basico</div>
-                                <q-separator
-                                    class="q-mb-sm"
-                                    :dark="registrationData.selectedRateId == 'plan_basico_id'"
+                        <div class="row q-mb-sm">
+                            <div class="col">
+                                <q-input
+                                    filled
+                                    label="Nombre"
+                                    class="on-left"
+                                    v-model="registrationData.name"
+                                    :rules="[
+                                        val =>
+                                            val.length > 0 || 'El campo es obligatorio'
+                                    ]"
                                 />
-                                <div class="text-subtitle2">Texto punto 1</div>
-                                <div class="text-subtitle2 q-mt-sm">Texto punto 2</div>
-                                <div class="text-subtitle2 q-mt-sm">Texto punto 3</div>
-                                <div class="text-h5 text-bold q-mt-md">$ 2.50</div>
+                            </div>
+                            <div class="col">
+                                <q-input
+                                    filled
+                                    label="Apellido"
+                                    class="on-right"
+                                    v-model="registrationData.lastName"
+                                    :rules="[
+                                        val =>
+                                            val.length > 0 || 'El campo es obligatorio'
+                                    ]"
+                                />
                             </div>
                         </div>
-                        <div class="col-lg-4 col-xs-12 q-pa-sm">
-                            <div
-                                :class="
-                                    registrationData.selectedRateId == 'plan_cerovol_id'
-                                        ? 'rateTileSelected q-pa-md rounded-borders text-center'
-                                        : 'rateTile q-pa-md rounded-borders text-center'
-                                "
-                                @click="registrationData.selectedRateId = 'plan_cerovol_id'"
-                            >
-                                <i class="fas fa-ruler-combined fa-2x q-mb-sm"></i>
-                                <div class="text-h6 q-mb-sm text-bold">Plan Cero Volumen</div>
-                                <q-separator
-                                    class="q-mb-sm"
-                                    :dark="registrationData.selectedRateId == 'plan_cerovol_id'"
-                                />
-                                <div class="text-subtitle2">Texto punto 1</div>
-                                <div class="text-subtitle2 q-mt-sm">Texto punto 2</div>
-                                <div class="text-subtitle2 q-mt-sm">Texto punto 3</div>
-                                <div class="text-h5 text-bold q-mt-md">$ 3.00</div>
+                        <div class="row q-mb-sm">
+                            <div class="col">
+                                <q-input filled label="Celular" v-model="registrationData.phone"  mask="####-####" fill-mask :rules="[val =>val.length > 0 || 'El campo es obligatorio']"/>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-xs-12 q-pa-sm">
-                            <div class="bg-grey-3 q-pa-md rounded-borders text-center">
-                                <i class="fas fa-building fa-2x q-mb-md"></i>
-                                <div class="text-h6 q-mb-sm text-primary text-bold">
-                                    Plan Business
+                        <div class="row q-mb-sm">
+                            <div class="col">
+                                <q-select filled label="Sucursal de preferencia" :options="['Sucursal A', 'Sucursal B']" :rules="[val =>val.length > 0 || 'El campo es obligatorio']" v-model="registrationData.subsidiary"/>
+                            </div>
+                        </div>
+                        <div class="row q-mb-md">
+                            <div class="col-lg-4 col-xs-12 q-pa-sm">
+                                <div
+                                    :class="
+                                        registrationData.selectedRateId == 'plan_basico_id'
+                                            ? 'rateTileSelected q-pa-md rounded-borders text-center'
+                                            : 'rateTile q-pa-md rounded-borders text-center'
+                                    "
+                                    @click="registrationData.selectedRateId = 'plan_basico_id'"
+                                >
+                                    <i class="fas fa-weight fa-2x q-mb-sm"></i>
+                                    <div class="text-h6 q-mb-sm text-bold">Plan Basico</div>
+                                    <q-separator
+                                        class="q-mb-sm"
+                                        :dark="registrationData.selectedRateId == 'plan_basico_id'"
+                                    />
+                                    <div class="text-subtitle2">Texto punto 1</div>
+                                    <div class="text-subtitle2 q-mt-sm">Texto punto 2</div>
+                                    <div class="text-subtitle2 q-mt-sm">Texto punto 3</div>
+                                    <div class="text-h5 text-bold q-mt-md">$ 2.50</div>
                                 </div>
-                                <q-separator class="q-mb-sm" />
-                                <div class="text-subtitle2">Texto punto 1</div>
-                                <div class="text-subtitle2 q-mt-sm">Texto punto 2</div>
-                                <div class="text-subtitle2 q-mt-sm">Texto punto 3</div>
-                                <q-btn
-                                    class="q-mt-md"
-                                    label="Contactar"
-                                    size="sm"
-                                    color="accent"
-                                    push
+                            </div>
+                            <div class="col-lg-4 col-xs-12 q-pa-sm">
+                                <div
+                                    :class="
+                                        registrationData.selectedRateId == 'plan_cerovol_id'
+                                            ? 'rateTileSelected q-pa-md rounded-borders text-center'
+                                            : 'rateTile q-pa-md rounded-borders text-center'
+                                    "
+                                    @click="registrationData.selectedRateId = 'plan_cerovol_id'"
+                                >
+                                    <i class="fas fa-ruler-combined fa-2x q-mb-sm"></i>
+                                    <div class="text-h6 q-mb-sm text-bold">Plan Cero Volumen</div>
+                                    <q-separator
+                                        class="q-mb-sm"
+                                        :dark="registrationData.selectedRateId == 'plan_cerovol_id'"
+                                    />
+                                    <div class="text-subtitle2">Texto punto 1</div>
+                                    <div class="text-subtitle2 q-mt-sm">Texto punto 2</div>
+                                    <div class="text-subtitle2 q-mt-sm">Texto punto 3</div>
+                                    <div class="text-h5 text-bold q-mt-md">$ 3.00</div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-xs-12 q-pa-sm">
+                                <div class="bg-grey-3 q-pa-md rounded-borders text-center">
+                                    <i class="fas fa-building fa-2x q-mb-md"></i>
+                                    <div class="text-h6 q-mb-sm text-primary text-bold">
+                                        Plan Business
+                                    </div>
+                                    <q-separator class="q-mb-sm" />
+                                    <div class="text-subtitle2">Texto punto 1</div>
+                                    <div class="text-subtitle2 q-mt-sm">Texto punto 2</div>
+                                    <div class="text-subtitle2 q-mt-sm">Texto punto 3</div>
+                                    <q-btn
+                                        class="q-mt-md"
+                                        label="Contactar"
+                                        size="sm"
+                                        color="accent"
+                                        push
+                                    />
+                                </div>
+                            </div>
+                            <div class="text-body2 full-width text-red-8 text-bold q-px-sm" v-if="true">
+                                *** Debes seleccionar una tarifa ***
+                            </div>
+                        </div>
+                        <div class="row q-mb-sm">
+                            <div class="col">
+                                <q-input
+                                    type="textarea"
+                                    filled
+                                    rows="4"
+                                    label="Direccion de entrega"
+                                    v-model="registrationData.address"
+                                    :rules="[val =>val.length > 0 || 'El campo es obligatorio']"
                                 />
                             </div>
                         </div>
-                    </div>
-                    <div class="row q-mb-md">
-                        <div class="col">
-                            <q-input
-                                type="textarea"
-                                filled
-                                rows="4"
-                                label="Direccion de entrega"
-                                v-model="registrationData.address"
-                            />
+                        <div class="row q-mb-md">
+                            <div class="col">
+                                <GoogleMaps
+                                    class="q-mb-md"
+                                    @markerPosition="setMarkerPosition"
+                                    :editable="true"
+                                    :markers="markers"
+                                    :mapCenter="center"
+                                ></GoogleMaps>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <q-space />
+                            <q-btn color="accent" label="Continuar" push type="submit" />
                         </div>
                     </div>
-                    <div class="row q-mb-md">
-                        <div class="col">
-                            <GoogleMaps
-                                class="q-mb-md"
-                                @markerPosition="setMarkerPosition"
-                                :editable="true"
-                                :markers="markers"
-                                :mapCenter="center"
-                            ></GoogleMaps>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <q-space />
-                        <q-btn color="accent" label="Continuar" push @click="submitForm()" />
-                    </div>
+                    <div class="col desktop-only"></div>
                 </div>
-                <div class="col desktop-only"></div>
-            </div>
-        </q-card-section>
+            </q-card-section>
+        </q-form>
     </q-card>
 </template>
 
@@ -164,6 +178,7 @@ export default {
                 phone: '',
                 selectedRateId: '',
                 address: '',
+                subsidiary: ''
             },
             location: [],
             markers: [],
