82c770cae846d3b140091f88c1cd2b8b8b467158
Alejandro Millan <darkslave@live.com>
2021-03-18 13:55:41 -0500

    navbar fix

--- src/components/general/Navbar.vue
+++ src/components/general/Navbar.vue
@@ -2,7 +2,7 @@
     <div class="navigation-bar bg-white text-white" style="padding: 5px 0">
         <div class="row">
             <q-space />
-            <div class="col-lg-7 col-md-10 col-sm-12 col-xs-12 q-px-md">
+            <div class="col-lg-9 col-md-10 col-sm-12 col-xs-12 q-px-md">
                 <div class="row full-width q-my-sm">
                     <div class="col-lg-1 col-md-1 col-sm-12 col-xs-3 column flex-center">
                         <q-img :src="require('@/assets/logo_ygo.png')" />
